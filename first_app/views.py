73dcd38401f10b9c26804c96685d5dd1ad3ade9f
shufolislam1 <shufolislam1255@gmail.com>
2023-12-12 18:44:02 +0600

    all done

--- first_app/views.py
+++ first_app/views.py
@@ -3,5 +3,11 @@ from . import forms
 
 # Create your views here.
 def home(request):
-    std = forms.studentForm
-    return render(request,'home.html', {'data': std})
+    if request.method == 'POST':
+        form = forms.studentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            print(form.cleaned_data)
+    else:
+        form = forms.studentForm()
+    return render(request,'home.html', {'data': form})
